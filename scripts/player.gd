dfd4b9185e55edce694ef2d2424362558a058e7c
AflamePetal <matin.ebrahimpour81@gmail.com>
2026-09-10 11:33:20 +0330

    Character boundry controlled

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,7 @@
-extends Node2D
+extends CharacterBody2D
 
 const MAP_RECT := Rect2(0, 0, 384, 640)
 const SPEED: int = 200
-const PLAYER_HALF_WIDTH := 6.0
-const PLAYER_HALF_HEIGHT := 7.0
 
 var direction: Vector2 = Vector2.ZERO
 
@@ -15,16 +13,6 @@ func _physics_process(delta: float) -> void:
 		"move_down"
 	)
 
-	position += direction * SPEED * delta
-
-	position.x = clamp(
-		position.x,
-		MAP_RECT.position.x + PLAYER_HALF_WIDTH,
-		MAP_RECT.end.x - PLAYER_HALF_WIDTH
-	)
-
-	position.y = clamp(
-		position.y,
-		MAP_RECT.position.y + PLAYER_HALF_HEIGHT,
-		MAP_RECT.end.y - PLAYER_HALF_HEIGHT
-	)
+	velocity = direction * SPEED
+	
+	move_and_slide()
